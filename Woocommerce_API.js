df480716a2237a2abc4da87eabf9c7c32b250e71
johannmanoj <johannmanoj97@gmail.com>
2023-07-18 12:22:50 +0530

    woocommerce auth modification

--- Woocommerce_API.js
+++ Woocommerce_API.js
@@ -1,30 +1,12 @@
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 
-
-
-
-
-// gets woo credentials from database
-const woo_get_auth = async(userid) => {
-    auth_row = await db.GetWooCommerceUserCredentials(userid)
-    if (auth_row.data.length > 0){
-        return auth_row.data[0]
-    } else{
-        return null
-    }
-}
-
-const get_wcapi_object = async (personid=7) => {
-    const auth_details = await woo_get_auth(personid)
-    
-    if (auth_details == null){
-        return null
-    }
+//--Woocommerce auth--
+const get_wcapi_object = async (STORE_URL, CONSUMER_KEY, CONSUMER_SECRET) => {
     const wcapi = new WooCommerceRestApi({
-        url: auth_details["store_url"],
-        consumerKey: auth_details["consumer_key"],
-        consumerSecret: auth_details["consumer_secret"], 
+        url: STORE_URL,
+        consumerKey: CONSUMER_KEY,
+        consumerSecret: CONSUMER_SECRET, 
         version: 'wc/v3', 
         queryStringAuth: true 
     });
