bd45a190d022e91a8644da4a82dcabddce24c096
johannmanoj <johannmanoj97@gmail.com>
2023-07-20 07:45:13 +0530

    woocommerce products api modification

--- Woocommerce_API.js
+++ Woocommerce_API.js
@@ -1,5 +1,9 @@
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
+const STORE_URL = ""
+const CONSUMER_KEY = ""
+const CONSUMER_SECRET = ""
+
 
 //--Woocommerce auth--
 const get_wcapi_object = async (STORE_URL, CONSUMER_KEY, CONSUMER_SECRET) => {
@@ -14,8 +18,9 @@ const get_wcapi_object = async (STORE_URL, CONSUMER_KEY, CONSUMER_SECRET) => {
     return wcapi
 }
 
-const getAllOrders = async (user_id) => {
-    Woo = await get_wcapi_object(user_id)
+//--Retrieves Orders list--
+const getAllOrders = async (STORE_URL, CONSUMER_KEY, CONSUMER_SECRET) => {
+    Woo = await get_wcapi_object(STORE_URL, CONSUMER_KEY, CONSUMER_SECRET)
     let allOrders = await Woo.get("orders")
     .then((response) => {
         return response.data;
@@ -23,8 +28,33 @@ const getAllOrders = async (user_id) => {
     .catch((error) => {
         return error.response.data;
     });
-    console.log(allOrders[1])
+
     return allOrders;
+}
+
+//--Retrieves Products list--
+const getAllProducts = async (STORE_URL, CONSUMER_KEY, CONSUMER_SECRET) => {
+    var page = 1;
+    var data = [];
+    wcapi = await get_wcapi_object(STORE_URL, CONSUMER_KEY, CONSUMER_SECRET)
+    
+    do {
+        var allProducts = await wcapi.get(`products?page=${page}`)
+        .then((response) => {
+            page += 1;
+            for(let i in response.data) {
+                data.push(response.data[i]);
+            }
+            // console.log(response.data[0])
+            return response.data;
+        })
+        .catch((error) => {
+            return error.response.data
+        });
+    } while (allProducts.length > 9);
+
+
+    return data;
 }
 
 
